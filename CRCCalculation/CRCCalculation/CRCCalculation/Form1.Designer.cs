6b2decf964f9aa05fa6b32ca045618ba0335f9e2
Maksim Zakharenka <mv.maxcriser@gmail.com>
2017-11-15 20:20:43 +0300

    CRC push

---
+++ CRCCalculation/CRCCalculation/CRCCalculation/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CRCCalculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.compareButton = new System.Windows.Forms.Button();
+            this.selectedFolderTextView = new System.Windows.Forms.TextBox();
+            this.selftCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Location = new System.Drawing.Point(13, 13);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(187, 23);
+            this.selectFolderButton.TabIndex = 0;
+            this.selectFolderButton.Text = "Select folder";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // compareButton
+            // 
+            this.compareButton.Location = new System.Drawing.Point(13, 43);
+            this.compareButton.Name = "compareButton";
+            this.compareButton.Size = new System.Drawing.Size(187, 23);
+            this.compareButton.TabIndex = 1;
+            this.compareButton.Text = "Compare with etalon";
+            this.compareButton.UseVisualStyleBackColor = true;
+            this.compareButton.Click += new System.EventHandler(this.compareButton_Click);
+            // 
+            // selectedFolderTextView
+            // 
+            this.selectedFolderTextView.Location = new System.Drawing.Point(13, 73);
+            this.selectedFolderTextView.Name = "selectedFolderTextView";
+            this.selectedFolderTextView.Size = new System.Drawing.Size(726, 20);
+            this.selectedFolderTextView.TabIndex = 2;
+            // 
+            // selftCheck
+            // 
+            this.selftCheck.Location = new System.Drawing.Point(207, 13);
+            this.selftCheck.Name = "selftCheck";
+            this.selftCheck.Size = new System.Drawing.Size(532, 54);
+            this.selftCheck.TabIndex = 3;
+            this.selftCheck.Text = "START SELF CHECK";
+            this.selftCheck.UseVisualStyleBackColor = true;
+            this.selftCheck.Click += new System.EventHandler(this.selftCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 106);
+            this.Controls.Add(this.selftCheck);
+            this.Controls.Add(this.selectedFolderTextView);
+            this.Controls.Add(this.compareButton);
+            this.Controls.Add(this.selectFolderButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.Button compareButton;
+        private System.Windows.Forms.TextBox selectedFolderTextView;
+        private System.Windows.Forms.Button selftCheck;
+    }
+}
+
---
+++ CRCCalculation/CRCCalculation/CRCCalculation/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CRCCalculation
+{
+    public partial class Form1 : Form
+    {
+        public static string SAVED_FILE_NAME = "D:\\crc_calculation.txt";
+        public static string SAVED_FILE_NAME_SELF_CHECK = "D:\\crc_calculation_self_check.txt";
+        public static string EXE_FILE_DIRECTORY = "D:\\GitHubProjects\\University5Sem\\CRCCalculation\\CRCCalculation\\CRCCalculation\\bin\\Debug";
+
+        public Form1()
+        {
+            InitializeComponent();
+            processSelfCheck();
+        }
+
+        private void processSelfCheck()
+        {
+            if (isCorrectCrc(SAVED_FILE_NAME_SELF_CHECK, getFilesArrayByDirectory(EXE_FILE_DIRECTORY)))
+            {
+                showToast("Selfcheck is correct");
+            } else
+            {
+                showToast("Selfxcheck is incorrect");
+            }
+        }
+
+        private string[] getFilesArrayByDirectory(string pDirectory)
+        {
+                return Directory.GetFiles(pDirectory, "*.*", System.IO.SearchOption.AllDirectories);
+        }
+
+        private string[] getFilesArray()
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    selectedFolderTextView.Text = fbd.SelectedPath;
+
+                    return Directory.GetFiles(fbd.SelectedPath, "*.*", System.IO.SearchOption.AllDirectories);
+                } else
+                {
+                    return null;
+                }
+            }
+        }
+
+        private String getCRCOfFile(string pFileName)
+        {
+            Crc32 crc32 = new Crc32();
+            String hash = String.Empty;
+
+            // if we use it for project we hav exception
+            try
+            {
+                    foreach (byte b in crc32.ComputeHash(File.ReadAllBytes(pFileName))) hash += b.ToString("x2").ToLower();
+            } catch
+            {
+
+            }
+
+            return hash;
+        }
+
+        private Dictionary<string, string> getCrcOfDirectory(string[] pArrayOfFileNames)
+        {
+            Dictionary<string, string> mapList = new Dictionary<string, string>();
+
+            for (int position = 0; position < pArrayOfFileNames.Length; position++)
+            {
+                string fileName = pArrayOfFileNames[position];
+
+                mapList.Add(fileName, getCRCOfFile(fileName));
+            }
+
+            return mapList;
+        }
+
+        private void processCRCCalculation(string[] pArrayOfFileNames, string pFileName)
+        {
+            writeToFile(getCrcOfDirectory(pArrayOfFileNames), pFileName);
+        }
+
+        private void writeToFile(Dictionary<string, string> pMapList, string pFileName)
+        {
+            string path = pFileName;
+
+            try
+            {
+                File.WriteAllText(path, String.Empty);
+            } catch
+            {
+                // nothing, its just first creation
+            }
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                for (int i = 0; i < pMapList.Count; i++)
+                {
+                    sw.WriteLine(pMapList.Keys.ElementAt(i));
+                    sw.WriteLine(pMapList.Values.ElementAt(i));
+                }
+            }
+        }
+
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            processCRCCalculation(getFilesArray(), SAVED_FILE_NAME);
+        }
+
+        private Dictionary<string, string> getSavedCrcStatus(string pDirectory)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            string[] lines = System.IO.File.ReadAllLines(pDirectory);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                result.Add(lines[i], lines[++i]);
+            }
+
+            return result;
+        }
+
+        private Boolean isCorrectCrc(string pDirectory, string[] pFiles) 
+        {
+            Dictionary<string, string> currentCompareVersion = getCrcOfDirectory(pFiles);
+            Dictionary<string, string> savedCompareVersion = getSavedCrcStatus(pDirectory);
+
+            return currentCompareVersion.SequenceEqual(savedCompareVersion);
+        }
+
+        private void showToast(string pText)
+        {
+            MessageBox.Show(pText, "Result", MessageBoxButtons.OK);
+        }
+
+        private void compareButton_Click(object sender, EventArgs e)
+        {
+            if (isCorrectCrc(SAVED_FILE_NAME, System.IO.Directory.GetFiles(selectedFolderTextView.Text, "*.*", System.IO.SearchOption.AllDirectories)))
+            {
+                showToast("Correct");
+            } else
+            {
+                showToast("Incorrect");
+            }
+        }
+
+        private void selftCheck_Click(object sender, EventArgs e)
+        {
+            processCRCCalculation(getFilesArrayByDirectory(EXE_FILE_DIRECTORY), SAVED_FILE_NAME_SELF_CHECK);
+
+            Application.Exit();
+        }
+    }
+}
